863ed1c6954ec1ce29522fea7001617d8d0dfde9
aspen <jgromadzinska9@gmail.com>
2023-05-24 12:45:01 +0100

    Store book objects in an array

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const addBtn = document.getElementById("add-book")
 const formContainer = document.getElementById("form-container")
 const submitBtn = document.getElementById("submit")
+const form = document.getElementsByTagName("form")
+
+//FORM DATA
+let title;
+let author;
+let pages;
+let year;
+let status;
+
+function getFormData() {
+    title = document.getElementById("title").value
+    author = document.getElementById("author").value
+    pages = document.getElementById("pages").value
+    year = document.getElementById("year").value
+    status = document.getElementById("status").value
+  }
 
 let myLibrary = [];
 
@@ -8,23 +24,22 @@ let myLibrary = [];
 function Book(title, author, pages, year, status) {
     //the constructor
     this.title = title
-    this.author = author
-    this.pages = pages
-    this.year = year
+    this.author = author || "Unknown"
+    this.pages = pages || "Unknown"
+    this.year = year  || "Unknown"
     this.status = status
 
 };
 
-//dummy books to test the constructor code
-const hp1 = new Book("Harry Potter and the Philosopher's Stone",
-"J. K. Rowling", 233, 1997, "Read");
 
 function addBookToLibrary() {
-
+    getFormData();
+    const book = new Book(title, author, pages, year, status);
+    //console.table(book);
+    myLibrary.push(book);
 };
 
 //POP-UP FORM
-
 function showForm () {
     formContainer.classList.remove("invisible-form");
 };
@@ -35,8 +50,12 @@ addBtn.addEventListener("click", function () {
 
 function hideForm () {
     formContainer.classList.add("invisible-form");
-}
+};
 
 submitBtn.addEventListener("click", function() {
+    //stop form submission
+    event.preventDefault();
     hideForm()
-})
+    addBookToLibrary();
+});
+
